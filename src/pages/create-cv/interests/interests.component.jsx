6b236b99403abebbea0d375d75846d4278b456b7
hossam <frontend@flows.sa>
2020-11-29 19:58:34 +0200

    updated CKEditor and removing Swjy and Jodit Editor

--- src/pages/create-cv/interests/interests.component.jsx
+++ src/pages/create-cv/interests/interests.component.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Title } from "./interests.styles";
-
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { Button } from "react-bootstrap";
 const Interests = () => {
+  const [state, setState] = useState({ content: "" });
+
+  const { content } = state;
+
+  const HandleCkEditorState = (event, editor) => {
+    const data = editor.getData();
+    setState({ content: data });
+  };
+
+  console.log(state, `here is State =>>>>>>>>>`);
+
   return (
     <div className="container">
       <Title> Interests </Title>
+
+      <CKEditor
+        editor={ClassicEditor}
+        onInit={(editor) => {}}
+        onChange={HandleCkEditorState}
+      />
+      <input className="btn btn info " type="submit" />
     </div>
   );
 };

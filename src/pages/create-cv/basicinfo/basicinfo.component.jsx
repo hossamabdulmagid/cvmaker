d74904ba0971b8fe5dfa2180fb86e998ad445ac9
hossam <frontend@flows.sa>
2020-12-06 22:12:34 +0200

    update section view

--- src/pages/create-cv/basicinfo/basicinfo.component.jsx
+++ src/pages/create-cv/basicinfo/basicinfo.component.jsx
@@ -119,43 +119,42 @@ const BasicInfo = (props) => {
         console.log(error, `there is was an error`);
       });
   }, [currentUser, id]);
-  /*
-    const [image, setImage] = useState(null);
-    const [url, setUrl] = useState("");
-    const [progress, setProgress] = useState(0);
-  
-    const handleChangeImage = (e) => {
-      if (e.target.files[0]) {
-        setImage(e.target.files[0]);
+
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const handleChangeImage = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+          });
       }
-    };
-    const handleUpload = () => {
-      const uploadTask = storage.ref(`images/${image.name}`).put(image);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
-          setProgress(progress);
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          storage
-            .ref("images")
-            .child(image.name)
-            .getDownloadURL()
-            .then((url) => {
-              setUrl(url);
-            });
-        }
-      );
-    };
-        z 
-    console.log("image :", image);
-   */
+    );
+  };
+
+  console.log("image :", image);
 
   const [color, setColor] = React.useState("");
 
@@ -171,22 +170,30 @@ const BasicInfo = (props) => {
   const [sectionName, setSectionName] = useState({
     sectionlabel: "",
   });
-
-  const onSubmitSectionLabel = async (value) => {
-    await firestore
-      .collection(`users/${currentUser.id}/cvs`)
-      .doc(`${id}`)
-      .update("label", sectionName.sectionlabel);
-    toast({
-      title: "cv name updated.",
-      description: `your cvname updated  to : ${sectionName.sectionlabel} `,
-      status: "success",
-      duration: 5000,
-      isClosable: true,
-      position: "top-right",
-    });
+  let { sectionlabel } = sectionName;
+  const handleChangeSectionName = (event) => {
+    const { name, value } = event.target;
+    setSectionName({ ...sectionName, [name]: value });
   };
 
+  const onSubmitSectionLabel = async (value) => {
+    try {
+      await firestore
+        .collection(`users/${currentUser.id}/cvs`)
+        .doc(`${id}/data`)
+        .update("sectionName", sectionName.sectionlabel);
+      toast({
+        title: "section name updated.",
+        description: `your SectionName updated  to : ${sectionName.sectionlabel} `,
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    } catch (error) {
+      console.log(error, `errror`);
+    }
+  };
   return (
     <Fragment>
       <Container className="container-fluid">
@@ -247,7 +254,10 @@ const BasicInfo = (props) => {
                       value={dataform.address3}
                       onChange={handleChange}
                     />
-                    {errors.address3 && errors.address3.message}
+
+                    <small className="errorSectionName">
+                      {errors.address3 && errors.address3.message}
+                    </small>
                   </div>
                   <hr />
                   <div className="col-6">
@@ -274,9 +284,9 @@ const BasicInfo = (props) => {
                       value={dataform.webSites}
                       onChange={handleChange}
                     />
-
-                    {errors.websites && errors.websites.message}
-
+                    <small className="errorSectionName">
+                      {errors.websites && errors.websites.message}
+                    </small>
                     <hr />
                     <Label>Address Line 2</Label>
                     <Input
@@ -296,27 +306,22 @@ const BasicInfo = (props) => {
                   <hr />
                 </div>
 
-                {/* <div className="row">
-                <div className="col-6">
-                  <Upload id="" type="file" onChange={handleChangeImage} />
-                  <br />
-                  <img
-                    src={url || "http://via.placeholder.com/100"}
-                    alt="firebase-image"
-                  />
+                <div className="row">
+                  <div className="col-6">
+                    <Upload id="" type="file" onChange={handleChangeImage} />
+                    <br />
+                    <img
+                      src={url || "http://via.placeholder.com/100"}
+                      alt="firebase-image"
+                    />
+                  </div>
+                  <div className="col-6">
+                    <Buttons onClick={handleUpload}>Upload</Buttons>
+                    <br />
+                    <br />
+                    {url.length > 5 ? null : null}
+                  </div>
                 </div>
-                <div className="col-6">
-                  <Buttons onClick={handleUpload}>Upload</Buttons>
-                  <br />
-                  <br />
-                  {url.length > 5 ? (
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                      Show you image
-                    </a>
-                  ) : null}
-                </div>
-              </div>
-              */}
               </div>
             </form>
           </>
@@ -346,9 +351,9 @@ const BasicInfo = (props) => {
               <Input
                 placeholder="SectionName"
                 type="text"
-                name="section"
-                // value={sectionData.section}
-                //onChange={handleChangeSection}
+                name="sectionlabel"
+                value={sectionlabel}
+                onChange={handleChangeSectionName}
                 ref={register({
                   required: "*input is required",
                 })}
@@ -362,7 +367,7 @@ const BasicInfo = (props) => {
                 variantColor="blue"
                 mr={3}
                 type="submit"
-                className={`buttonForSaveNewSection`}
+                className="buttonForSaveNewSection"
                 onOpen
               >
                 Save
